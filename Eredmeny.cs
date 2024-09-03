d08c676d4a556415d5403a89afa213f0071ce191
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-09-03 13:17:37 +0200

    Add project files.

---
+++ Eredmeny.cs
@@ -0,0 +1,19 @@
+﻿namespace HelsinkiCLI
+{
+    internal class Eredmeny
+    {
+        public int Helyzes { get; set; }
+        public int Sportolok { get; set; }
+        public string Sportag { get; set; }
+        public string Versenyszam { get; set; }
+
+        public Eredmeny(string sor)
+        {
+            string[] v = sor.Split(' ');
+            Helyzes = int.Parse(v[0]);
+            Sportolok = int.Parse(v[1]);
+            Sportag = v[2];
+            Versenyszam = v[3];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using HelsinkiCLI;
+
+List<Eredmeny> eredmenyek = [];
+
+using StreamReader sr = new(@"..\..\..\src\helsinki.txt");
+
+while (!sr.EndOfStream) eredmenyek.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"ennyi sor van: {eredmenyek.Count}");
